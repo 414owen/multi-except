3fd96f727efc576a9d25c5bcd8757673386e3fb2
Owen Shepherd <owen@owen.cafe>
2021-05-12 16:21:21 +0100

    added nix with niv

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ mkDerivation
+, stdenv
+, base
+, dlist_1_0
+, semigroupoids
+, haskell-language-server
+}:
+
+mkDerivation {
+  pname = "multi-except";
+  version = "0.1.1.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    base
+    dlist_1_0
+    semigroupoids
+  ];
+  buildTools = [
+    haskell-language-server
+  ];
+  license = "MIT";
+  hydraPlatforms = stdenv.lib.platforms.none;
+}
