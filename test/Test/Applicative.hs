d7c05d7331eda42df4d73e100b5d511cc4100844
Owen Shepherd <owen@owen.cafe>
2021-05-29 20:59:40 +0000

    added success/error applicative tests

--- test/Test/Applicative.hs
+++ test/Test/Applicative.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TupleSections #-}
 
 module Test.Applicative
   ( spec
@@ -10,7 +11,7 @@ import Test.Hspec
 import Control.Applicative.MultiExcept
 
 testErrors :: MultiExcept Int Int
-testErrors = fromEitherPoly (Left [2, 3])
+testErrors = fromEitherPoly $ Left [2, 3]
 
 spec :: Spec
 spec = describe "Applicative instance" $ do
@@ -22,3 +23,6 @@ spec = describe "Applicative instance" $ do
       throwError 2 <*> throwError 3 `shouldBe` testErrors
     it "propagates successes" $
       (,) <$> pure 3 <*> pure 4 `shouldBe` succeed @() (3, 4)
+    it "errors when only one side is successful" $ do
+      succeed (+ 1) <*> testErrors `shouldBe` testErrors
+      throwError 2 <*> succeed () `shouldBe` throwError @Int 2
