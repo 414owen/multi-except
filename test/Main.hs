29ae32a61ca1b8d9a50fafd8b3210c32da15761d
Owen Shepherd <owen@owen.cafe>
2021-05-31 17:17:22 +0000

    added bifunctor tests

--- test/Main.hs
+++ test/Main.hs
@@ -5,6 +5,7 @@ import Test.Hspec
 import qualified Test.Applicative
 import qualified Test.Alt
 import qualified Test.Functor
+import qualified Test.Bifunctor
 
 main :: IO ()
 main = hspec spec
@@ -14,3 +15,4 @@ spec = do
   Test.Functor.spec
   Test.Applicative.spec
   Test.Alt.spec
+  Test.Bifunctor.spec
---
+++ test/Test/Bifunctor.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Test.Bifunctor
+  ( spec
+  ) where
+
+import Test.Hspec
+
+import Data.Bifunctor
+import Control.Applicative.MultiExcept
+
+spec :: Spec
+spec = describe "Bifunctor instance" $ do
+  it "maps errors with first" $
+    first (+ 1) (throwErrors [3, 4]) `shouldBe` throwErrors @() [4, 5]
+  it "maps successes with second" $
+    second (+ 1) (succeed 3) `shouldBe` succeed @() 4
