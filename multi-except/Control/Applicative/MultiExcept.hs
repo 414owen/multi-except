48ab9dfd63adf5b36ec04dc7d61268a7a8624dac
Owen Shepherd <owen@owen.cafe>
2023-12-27 00:34:43 +0100

    feat: Remove dependence on any dlist library

--- multi-except/Control/Applicative/MultiExcept.hs
+++ multi-except/Control/Applicative/MultiExcept.hs
@@ -23,33 +23,81 @@ module Control.Applicative.MultiExcept
   , throwErrors
   ) where
 
-import Prelude (Eq, Ord, Either(..), Read, Show, Applicative(..), (.), ($))
+import Prelude (Eq(..), Ord(..), Either(..), Applicative(..), (.), ($), id, Show(..), (++))
 
 import Data.Functor         (Functor(..), (<$>))
 import Data.Foldable        (Foldable(..))
 import Data.Traversable     (Traversable(..))
-import Data.Semigroup       (Semigroup(..))
+#if MIN_VERSION_base(4,9,0)
+import Data.List.NonEmpty   (NonEmpty(..))
+#endif
 import Data.Bifunctor
-import Data.DList.DNonEmpty (DNonEmpty)
+
+
+-- NonEmptyDList
+
+-- | This is written here because:
+-- * The version in dlist is currently limited to ghc>=8.0
+-- * The version in dlist-nonempty is too heavy on dependencies
+-- * We only need a few trivial features anyway
+
+data NonEmptyDList a = NonEmptyDList !a !([a] -> [a])
+
+{-# INLINE nedlSingleton #-}
+nedlSingleton :: a -> NonEmptyDList a
+nedlSingleton a = NonEmptyDList a id
+
+{-# INLINE runNonEmptyDList #-}
+runNonEmptyDList :: NonEmptyDList a -> (a, [a])
+runNonEmptyDList (NonEmptyDList x xs) = (x, xs [])
+
+instance Eq a => Eq (NonEmptyDList a) where
+  NonEmptyDList x xs == NonEmptyDList y ys = x : xs [] == y : ys []
+
+instance Ord a => Ord (NonEmptyDList a) where
+  NonEmptyDList x xs `compare` NonEmptyDList y ys = (x : xs []) `compare` (y : ys [])
+
+instance Show a => Show (NonEmptyDList a) where
+  show nedl = case runNonEmptyDList nedl of
+    (x, xs) -> show $ x : xs
+
+appendNedl :: NonEmptyDList a -> NonEmptyDList a -> NonEmptyDList a
+appendNedl (NonEmptyDList x xs) (NonEmptyDList y ys) = NonEmptyDList x $ xs . (y:) . ys
+
+-- WARNING: O(n) space
+-- TODO Make this constant space
+instance Functor NonEmptyDList where
+  fmap f nedl = case runNonEmptyDList nedl of
+    (x, xs) -> NonEmptyDList (f x) (fmap f xs ++)
 
 -- | A 'MultiExcept' is a success value, or one or more errors.
 data MultiExcept err a
   = Success !a
-  | Errors !(DNonEmpty err)
-  deriving (Eq, Ord, Read, Show)
+  | Errors !(NonEmptyDList err)
+  deriving (Eq, Ord, Show)
 
 -- | Run the computation.
-runMultiExcept :: MultiExcept err a -> Either (DNonEmpty err) a
+runMultiExcept :: MultiExcept err a -> Either (NonEmptyDList err) a
 runMultiExcept (Errors errs) = Left errs
 runMultiExcept (Success a) = Right a
 
 -- | Throw a single error.
 throwError :: forall a err. err -> MultiExcept err a
-throwError = Errors . pure
+throwError = Errors . nedlSingleton
+
+#if MIN_VERSION_base(4,9,0)
 
 -- | Throw one or more errors.
-throwErrors :: forall a err. DNonEmpty err -> MultiExcept err a
-throwErrors = Errors
+throwErrors :: forall a err. NonEmpty err -> MultiExcept err a
+throwErrors (err :| errs) = Errors $ NonEmptyDList err (errs ++)
+
+#else
+
+-- | Throw one or more errors.
+throwErrors :: forall a err. (err, [err]) -> MultiExcept err a
+throwErrors (err, errs) = Errors $ NonEmptyDList err (errs ++)
+
+#endif
 
 -- | Embeds a value into a 'MultiExcept' context.
 succeed :: forall err a. a -> MultiExcept err a
@@ -60,10 +108,21 @@ fromEither :: Either err a -> MultiExcept err a
 fromEither (Left err) = throwError err
 fromEither (Right a) = Success a
 
+#if MIN_VERSION_base(4,9,0)
+
 -- | Convert a multi-error 'Either' to a 'MultiExcept'.
-fromEitherPoly :: Either (DNonEmpty err) a -> MultiExcept err a
-fromEitherPoly (Left errs) = Errors errs
+fromEitherPoly :: Either (NonEmpty err) a -> MultiExcept err a
+fromEitherPoly (Left errs) = throwErrors errs
 fromEitherPoly (Right a) = Success a
+
+#else
+
+-- | Convert a multi-error 'Either' to a 'MultiExcept'.
+fromEitherPoly :: Either (err, [err]) a -> MultiExcept err a
+fromEitherPoly (Left errs) = throwErrors errs
+fromEitherPoly (Right a) = Success a
+
+#endif
 
 -- | Join nested 'MultiExcept's with the same error type.
 --   Note that this doesn't imply a __useful__ 'Monad' instance.
@@ -76,6 +135,8 @@ instance Functor (MultiExcept err) where
   fmap f (Success a) = Success $ f a
   fmap _ (Errors errs) = Errors errs
 
+-- | WARNING: O(n) space and time in the length of the amount of errors
+-- this could be fixed by changing the difference list Functor instance.
 instance Bifunctor MultiExcept where
  bimap _ fa (Success a)    = Success $ fa a
  bimap ferr _ (Errors err) = Errors $ fmap ferr err
@@ -83,7 +144,7 @@ instance Bifunctor MultiExcept where
 instance Applicative (MultiExcept err) where
   pure = Success
 
-  Errors l <*> Errors l' = Errors $ l <> l'
+  Errors l <*> Errors l' = Errors $ appendNedl l l'
   Success f <*> Success a = Success $ f a
   Errors l <*> _ = Errors l
   _ <*> Errors l = Errors l
@@ -91,7 +152,7 @@ instance Applicative (MultiExcept err) where
 or :: MultiExcept err a -> MultiExcept err a -> MultiExcept err a
 Success a `or` _ = Success a
 _ `or` Success a = Success a
-Errors l `or` Errors r = Errors (l <> r)
+Errors l `or` Errors r = Errors $ appendNedl l r
 
 instance Foldable (MultiExcept err) where
   foldr f acc (Success a) = f a acc
