32f491ea3b4cc30fbc4b3c2e8a05000402b5e362
Owen Shepherd <owen@owen.cafe>
2023-12-27 01:46:19 +0100

    compat: Import foldable qualified in test

--- test/Test/MultiExcept/Foldable.hs
+++ test/Test/MultiExcept/Foldable.hs
@@ -4,11 +4,12 @@ module Test.MultiExcept.Foldable
 
 import Test.Hspec
 
+import qualified Data.Foldable as Foldable
 import Control.Applicative.MultiExcept
 
 spec :: Spec
 spec = describe "Foldable instance" $ do
   it "f is called for success" $
-    foldr (+) 2 (succeed 1) `shouldBe` 3
+    Foldable.foldr (+) 2 (succeed 1) `shouldBe` (3 :: Int)
   it "acc is returned for errors" $
-    foldr (+) 2 (throwError 1) `shouldBe` 2
+    Foldable.foldr (+) 2 (throwError (1 :: Int)) `shouldBe` (2 :: Int)
